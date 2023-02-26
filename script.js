129e5bd39860c32b0b541af6693b3854457e3397
Surieya <surieyasurieya@gmail.com>
2023-02-26 20:28:26 +0530

    second-commit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ recognition.addEventListener("result", (e) => {
     if (text.includes("how are you")) {
       p = document.createElement("p");
       p.classList.add("replay");
-      p.innerText = "I am fine";
+      p.innerText = "I am fine.. What About you?";
       texts.appendChild(p);
     }
     if (
@@ -29,17 +29,37 @@ recognition.addEventListener("result", (e) => {
     ) {
       p = document.createElement("p");
       p.classList.add("replay");
-      p.innerText = "My Name is Cifar";
+      p.innerText = "My Name is Eagle";
       texts.appendChild(p);
     }
-    if (text.includes("open my YouTube")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "opening youtube channel";
-      texts.appendChild(p);
-      console.log("opening youtube");
-      window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
+    // if (text.includes("open my YouTube")) {
+    //   p = document.createElement("p");
+    //   p.classList.add("replay");
+    //   p.innerText = "opening youtube channel";
+    //   texts.appendChild(p);
+    //   console.log("opening youtube");
+    //   window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
+    // }
+    if(text.includes("hi || Hi")){
+        p = document.createElement("p");
+        p.classList.add("replay");
+        p.innerText = "hi suriya how can I help you";
+        texts.appendChild(p);
     }
+    if(text.includes("hello" || "Hello")){
+        p = document.createElement("p");
+        p.classList.add("replay");
+        p.innerText = "hello saravanan how can I help you";
+        texts.appendChild(p);
+    }
+    if(text.includes("hey" || "Hey")){
+        p = document.createElement("p");
+        p.classList.add("replay");
+        p.innerText = "hello Thanush how can I help you";
+        texts.appendChild(p);
+    }
+
+    if(text)
     p = document.createElement("p");
   }
 });
